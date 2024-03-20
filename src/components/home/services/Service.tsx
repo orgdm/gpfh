00dc5b40de86c7fa827ca91ed1227b24cbde2b65
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2024-03-20 19:25:38 -0400

    new s3 bucket

---
+++ src/components/home/services/Service.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type ServiceProps = {
+  img: string;
+  title: string;
+  desc: string;
+  reverse: boolean;
+};
+
+const Service = ({ img, title, desc, reverse }: ServiceProps) => {
+  return <div>Service</div>;
+};
+
+export default Service;
