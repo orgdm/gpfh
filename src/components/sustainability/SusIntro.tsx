aee1ce90216ee08850f008bbf6114502acabbe49
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2024-04-23 18:16:25 -0400

    remove overflow-x hidden from body and chnage sus intro text to flex

--- src/components/sustainability/SusIntro.tsx
+++ src/components/sustainability/SusIntro.tsx
@@ -4,17 +4,12 @@ import type { IntroProps } from '@/types/sustainability';
 const SusIntro = ({ text, img }: IntroProps) => {
   return (
     <div className='relative before:absolute before:w-full before:h-full before:z-[1] before:bg-img-overlay overflow-hidden h-[calc((47*1vh)+2rem)]'>
-      <Image
-        src={img.url}
-        fill
-        alt={img.alt}
-        className='h-auto w-full object-cover'
-      />
-      <div className='absolute bottom-5 px-4 sm:px-6 py-2 md:px-8 z-[2]'>
-        <h1 className='my-[0.4em] text-4xl font-light md:text-5xl font-archivo tracking-wide xl:text-6xl '>
+      <Image src={img.url} fill alt={img.alt} className='w-full object-cover' />
+      <div className='h-full flex flex-col relative justify-end px-4 sm:px-6 py-2 md:px-8 z-[2]'>
+        <h1 className='my-[0.36em] text-4xl font-light md:text-5xl font-archivo tracking-wide xl:text-6xl '>
           {img.title}
         </h1>
-        <div>
+        <div className='pb-2'>
           <p className='font-extralight text-[1rem] md:text-[1.125rem] xl:text-[1.25rem] tracking-wide sm:px-1'>
             {text}
           </p>
