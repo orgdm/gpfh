4d978bdb9765e804df878fd05ece5b11291aa46c
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2024-04-24 18:39:18 -0400

    finally fix layout shift with open menu ??

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,109 +7,124 @@ import FullScreenNav from './navmenu/FullScreenNav';
 import CloseOnClick from './navmenu/CloseOnClick';
 
 const links = [
-  { title: 'projects', href: '/projects' },
-  { title: 'about', href: '/about' },
-  { title: 'process', href: '/process' },
-  { title: 'sustainability', href: '/sustainability' },
-  { title: 'people', href: '/people' },
-  { title: 'newsletter', href: '/newsletter' },
-  { title: 'contact', href: '/contact' },
+	{ title: 'projects', href: '/projects' },
+	{ title: 'about', href: '/about' },
+	{ title: 'process', href: '/process' },
+	{ title: 'sustainability', href: '/sustainability' },
+	{ title: 'people', href: '/people' },
+	{ title: 'newsletter', href: '/newsletter' },
+	{ title: 'contact', href: '/contact' },
 ];
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState<boolean>(false);
-  const [scrollDir, setScrollDir] = useState<number>(2);
+	const [menuOpen, setMenuOpen] = useState<boolean>(false);
+	const [scrollDir, setScrollDir] = useState<number>(2);
 
-  useEffect(() => {
-    menuOpen
-      ? document.documentElement.setAttribute('data-scroll', 'n')
-      : document.documentElement.setAttribute('data-scroll', 'y');
-  }, [menuOpen]);
+	useEffect(() => {
+		menuOpen
+			? document.documentElement.setAttribute('data-scroll', 'n')
+			: document.documentElement.setAttribute('data-scroll', 'y');
+	}, [menuOpen]);
 
-  useEffect(() => {
-    const thresh = 2;
-    let lastY: number = window.scrollY;
-    let ticking: boolean = false;
+	useEffect(() => {
+		const thresh = 2;
+		let lastY: number = window.scrollY;
+		let ticking: boolean = false;
 
-    const updateDir = () => {
-      const y: number = window.scrollY;
-      const initial: boolean = lastY < 8;
+		const updateDir = () => {
+			const y: number = window.scrollY;
+			const initial: boolean = lastY < 8;
 
-      if (Math.abs(y - lastY) < thresh) {
-        ticking = false;
-        return;
-      }
-      const dir: number = initial ? 2 : y > lastY ? 0 : 1;
-      setScrollDir(dir);
-      lastY = y > 0 ? y : 0;
-      ticking = false;
-    };
+			if (Math.abs(y - lastY) < thresh) {
+				ticking = false;
+				return;
+			}
+			const dir: number = initial ? 2 : y > lastY ? 0 : 1;
+			setScrollDir(dir);
+			lastY = y > 0 ? y : 0;
+			ticking = false;
+		};
 
-    const onScroll = () => {
-      if (!ticking) {
-        window.requestAnimationFrame(updateDir);
-        ticking = true;
-      }
-    };
+		const onScroll = () => {
+			if (!ticking) {
+				window.requestAnimationFrame(updateDir);
+				ticking = true;
+			}
+		};
 
-    window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener('scroll', onScroll);
-  }, [scrollDir]);
+		window.addEventListener('scroll', onScroll);
+		return () => window.removeEventListener('scroll', onScroll);
+	}, [scrollDir]);
 
-  return (
-    <>
-      <header
-        className={`fixed z-50 left-0 right-0 top-0 bottom-auto transition-[transform] bg-head-overlay duration-300 ease-io-head bg-scroll ${
-          menuOpen
-            ? ' translate-y-0'
-            : scrollDir > 0
-            ? ' translate-y-0'
-            : ' -translate-y-full'
-        } `}
-      >
-        <div
-          className={`relative px-4 md:px-6 h-head transition-[background-color] ease-linear duration-150  ${
-            scrollDir < 2 ? 'delay-500 bg-stone-900 ' : ' bg-transparent'
-          }`}
-        >
-          <div className='flex w-full h-full flex-row justify-between xxl:grid xxl:grid-cols-12 xxl:grid-rows-1'>
-            <div className='flex items-center list-none relative xxl:col-start-1 xxl:col-end-3'>
-              <HomeLinkLogo />
-            </div>
-            <ToggleBtn open={menuOpen} setOpen={setMenuOpen} />
-            <ul className='hidden xxl:col-start-3 xxl:col-end-7 xxl:flex xxl:items-center '>
-              {links.slice(0, 5).map((link, index) => (
-                <li key={index} className='me-6'>
-                  <Link className='link' href={link.href}>
-                    <span className='uppercase font-archivo tracking-wide text-sm font-light'>
-                      {link.title}
-                    </span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-            <ul className='hidden xxl:col-start-9 xxl:col-end-13 xxl:flex xxl:items-center xxl:justify-self-end '>
-              {links.slice(5, 7).map((link, index) => (
-                <li className='me-6' key={index}>
-                  <Link className='link' href={link.href}>
-                    <span className='uppercase font-archivo tracking-wide text-sm  font-light'>
-                      {link.title}
-                    </span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className='block xxl:hidden'>
-          <FullScreenNav setOpen={setMenuOpen} open={menuOpen} />
-        </div>
-        {/* <div className='block xxl:hidden'>
-          <CloseOnClick setOpen={setMenuOpen} open={menuOpen} />
-        </div> */}
-      </header>
-    </>
-  );
+	return (
+		<>
+			<header
+				className={`fixed z-50 left-0 right-0 top-0 bottom-auto transition-[transform] bg-head-overlay duration-300 ease-io-head bg-scroll ${
+					menuOpen
+						? ' translate-y-0'
+						: scrollDir > 0
+						? ' translate-y-0'
+						: ' -translate-y-full'
+				} `}>
+				<div
+					className={`relative px-4 md:px-6 h-head transition-[background-color] ease-linear duration-150  ${
+						scrollDir < 2 ? 'delay-500 bg-stone-900 ' : ' bg-transparent'
+					}`}>
+					<div className='flex w-full h-full flex-row justify-between xxl:grid xxl:grid-cols-12 xxl:grid-rows-1'>
+						<div className='flex items-center list-none relative xxl:col-start-1 xxl:col-end-3'>
+							<HomeLinkLogo />
+						</div>
+						<ToggleBtn
+							open={menuOpen}
+							setOpen={setMenuOpen}
+						/>
+						<ul className='hidden xxl:col-start-3 xxl:col-end-7 xxl:flex xxl:items-center '>
+							{links.slice(0, 5).map((link, index) => (
+								<li
+									key={index}
+									className='me-6'>
+									<Link
+										className='link'
+										href={link.href}>
+										<span className='uppercase font-archivo tracking-wide text-sm font-light'>
+											{link.title}
+										</span>
+									</Link>
+								</li>
+							))}
+						</ul>
+						<ul className='hidden xxl:col-start-9 xxl:col-end-13 xxl:flex xxl:items-center xxl:justify-self-end '>
+							{links.slice(5, 7).map((link, index) => (
+								<li
+									className='me-6'
+									key={index}>
+									<Link
+										className='link'
+										href={link.href}>
+										<span className='uppercase font-archivo tracking-wide text-sm  font-light'>
+											{link.title}
+										</span>
+									</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
+				<div className='block xxl:hidden'>
+					<FullScreenNav
+						setOpen={setMenuOpen}
+						open={menuOpen}
+					/>
+				</div>
+				<div className='block xxl:hidden'>
+					<CloseOnClick
+						setOpen={setMenuOpen}
+						open={menuOpen}
+					/>
+				</div>
+			</header>
+		</>
+	);
 };
 
 export default Header;
--- src/components/layout/navmenu/FullScreenNav.tsx
+++ src/components/layout/navmenu/FullScreenNav.tsx
@@ -4,54 +4,53 @@ import { pageLinks } from '@/lib/misc';
 import { memo } from 'react';
 
 const baseStyle =
-  'fixed top-0 bottom-auto right-0 w-full transition-[transform] z-20 duration-500 ease-in-out';
+	'fixed top-0 bottom-auto right-0 w-full transition-[transform] duration-500 ease-in-out';
 
 const FullScreenNav = memo(function FullScreenNav({
-  open,
-  setOpen,
+	open,
+	setOpen,
 }: FullScreenNavProps) {
-  return (
-    <nav
-      className={
-        `${baseStyle}` +
-        `${
-          open
-            ? ' translate-y-0 pointer-events-auto overflow-hidden'
-            : ' -translate-y-full pointer-events-none overflow-y-auto'
-        }`
-      }
-    >
-      <div className='relative bg-scroll bg-stone-900 bg-origin-padding'>
-        <div className='w-full h-screen flex justify-end relative pointer-events-none'>
-          <div className={`w-full  pointer-events-auto`}>
-            <div
-              className={` mt-[9em] mb-10 transition-[opacity] duration-300 delay-500 ease-in-out ${
-                open ? ' opacity-100' : '  opacity-0 '
-              }`}
-            >
-              <menu className='flex flex-col w-full pointer-events-auto px-8 h-[calc(1vh*100+4rem)]'>
-                {pageLinks.map((link, index) => (
-                  <li key={index} className={`w-full py-3 flex justify-end `}>
-                    <Link
-                      className={`link`}
-                      onClick={() => setOpen(false)}
-                      href={link.href}
-                    >
-                      <span
-                        className={`text-xl font-light uppercase font-archivo tracking-wide`}
-                      >
-                        {link.title}
-                      </span>
-                    </Link>
-                  </li>
-                ))}
-              </menu>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<nav
+			className={
+				`${baseStyle}` +
+				`${
+					open
+						? ' translate-y-0 pointer-events-auto overflow-hidden'
+						: ' -translate-y-full pointer-events-none'
+				}`
+			}>
+			<div className='relative bg-scroll bg-origin-padding'>
+				<div className='w-full h-screen flex justify-end relative pointer-events-none'>
+					<div
+						className={`w-full bg-stone-900  z-20 md:w-[45%]  pointer-events-auto`}>
+						<div
+							className={` mt-[9em] mb-10 transition-[opacity] duration-300 delay-500 ease-in-out ${
+								open ? ' opacity-100' : '  opacity-0 '
+							}`}>
+							<menu className='flex flex-col w-full pointer-events-auto px-8 h-[calc(1vh*100+4rem)]'>
+								{pageLinks.map((link, index) => (
+									<li
+										key={index}
+										className={`w-full py-3 flex justify-end `}>
+										<Link
+											className={`link`}
+											onClick={() => setOpen(false)}
+											href={link.href}>
+											<span
+												className={`text-xl font-light uppercase font-archivo tracking-wide`}>
+												{link.title}
+											</span>
+										</Link>
+									</li>
+								))}
+							</menu>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 });
 
 export default FullScreenNav;
